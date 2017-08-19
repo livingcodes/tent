5aa2ea486db0b6a949625b6d9816a45b202d20b8
Case <case.joyce@gmail.com>
2017-08-19 00:31:48 -0500

    Example of view data, view bag and model

---
+++ Tent/Pages/Example.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tent.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet() {
+            Message = "Model property from code-behind (PageModel)";
+        }
+    }
+}
