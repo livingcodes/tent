e7d8ee632e7feeec054e340bf0de546ae2c7dff8
Case <case.joyce@gmail.com>
2017-09-10 18:27:36 -0500

    + hard login validation + add some field validators + add validation messages

--- Tent/Pages/admin/Login.cshtml.cs
+++ Tent/Pages/admin/Login.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace Tent.Pages.admin
 {
     public class Login
     {
+        [Required, EmailAddress]
         public string Email { get; set; }
+
+        [Required, StringLength(32, MinimumLength = 8)]
         public string Password { get; set; }
     }
     public class LoginModel : PageModel
@@ -19,12 +23,16 @@ namespace Tent.Pages.admin
         public Login Login { get; set; }
 
         public async Task<IActionResult> OnPostAsync() {
+            // todo: required and length client-side validation doesn't prevent this http post
             if (!ModelState.IsValid)
                 return Page();
-
-            // todo: validate login
             
-            return RedirectToPage("/Index");
+            // todo: login using database
+            if (Login.Email == "admin@tent.com"
+            && Login.Password == "password")
+                return RedirectToPage("/Index");
+            
+            return Page();
         }
     }
 }
