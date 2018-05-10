d830a48dd63a93b384649c268cf7964db29547a3
Case <case.joyce@gmail.com>
2018-05-09 22:01:20 -0500

    /blog list posts

---
+++ Tent/Pages/blog/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Tent.Data;
+
+namespace Tent.Pages.blog
+{
+    public class IndexModel : BasePage
+    {
+        public void OnGet() {
+            var db = new Pack();
+            Posts = db.Select<Post>(
+                "SELECT * FROM Posts ORDER BY PublishDate DESC");
+        }
+
+        public List<Post> Posts { get; set; }
+    }
+}
