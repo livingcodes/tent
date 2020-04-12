be940d75576669fd574f7341afd01f96f6fce4f3
Case <case.joyce@gmail.com>
2020-04-12 14:01:40 -0500

    Make extension methods terse - define out parameter inline - lambda

--- Tent/Ase.Common/Extensions.cs
+++ Tent/Ase.Common/Extensions.cs
@@ -2,16 +2,14 @@
 {
     public static class Extensions
     {
-        public static string ToStringOr(this object value, string ifNull) {
-            return value == null
+        public static string ToStringOr(this object value, string ifNull) =>
+            value == null
                 ? ifNull
                 : value.ToString();
-        }
 
-        public static int ToInt(this string text, int @default = 0) {
-            int result = @default;
-            var succeeded = int.TryParse(text, out result);
-            return succeeded ? result : @default;
-        }
+        public static int ToInt(this string text, int @default = 0) =>
+            int.TryParse(text, out int result) 
+                ? result 
+                : @default;
     }
 }
