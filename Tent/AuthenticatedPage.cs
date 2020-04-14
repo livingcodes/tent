e2828e007566d5fed638be544bc952817e337b5e
Case <case.joyce@gmail.com>
2020-04-14 00:53:41 -0500

    Attempt to improve readability of authenticated page

--- Tent/AuthenticatedPage.cs
+++ Tent/AuthenticatedPage.cs
@@ -1,52 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using Tent.Logic;
+using static Shorthand;
 
 namespace Tent
 {
     public class AuthenticatedPage : PageModel
     {
-        public AuthenticatedPage(ICryptographer cryptographer) {
+        public AuthenticatedPage(ICryptographer cryptographer) =>
             this.cryptographer = cryptographer;
-        }
+
+        public UserCookie GetUserCookie() =>
+            Try(
+                () => userCookie._(decrypt)._(deserialize),
+                () => new UserCookie()
+            );
+        
+        public void SetUserCookie(UserCookie cookie) =>
+            setCookie( "user", cookie._(toJson)._(encrypt), Now.AddYears(1) );
+
+        public void SetUserCookie(User user) =>
+            user._(buildUserCookie)._(SetUserCookie);
+
+        #region private
         ICryptographer cryptographer;
-
-        public UserCookie GetUserCookie() {
-            var cookie = Request.Cookies["user"];
-            if (cookie == null)
-                return new UserCookie();
-
-            UserCookie deserialized = null;
-            try {
-                var unencryptedCookie = cryptographer.Decrypt(cookie);
-
-                deserialized = Newtonsoft.Json.JsonConvert.
-                    DeserializeObject<UserCookie>(unencryptedCookie);
-            } catch (Exception ex) {
-                return new UserCookie();
-            }
-
-            return deserialized;
-        }
-
-        public void SetUserCookie(UserCookie userCookie) {
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(userCookie);
-            var encryptedJson = cryptographer.Encrypt(json);
-            SetCookie("user", encryptedJson, DateTime.Now.AddYears(1));
-        }
-
-        public void SetUserCookie(User user) {
-            var userCookie = user == null
-                ? new UserCookie()
-                : new UserCookie() {
-                    Authenticated = true,
-                    Id = user.Id,
-                    Email = user.Email
-                };
-            SetUserCookie(userCookie);
-        }
-
-        void SetCookie(string key, string value, DateTime expiration) {
+        string userCookie => Request.Cookies["user"];
+        string toJson(object x) =>
+            Newtonsoft.Json.JsonConvert.SerializeObject(x);
+        string encrypt(string json) =>
+            cryptographer.Encrypt(json);
+        UserCookie deserialize(string json) =>
+            Newtonsoft.Json.JsonConvert.DeserializeObject<UserCookie>(json);
+        string decrypt(string cookie) =>
+            cryptographer.Decrypt(cookie); // exception if null (i.e. cookie missing)
+        void setCookie(string key, string value, DateTime expiration) =>
             Response.Cookies.Append(
                 key: key,
                 value: value,
@@ -54,11 +41,18 @@ namespace Tent
                     Expires = expiration
                 }
             );
-        }
+        UserCookie buildUserCookie(User user) =>
+            user == null
+                ? new UserCookie()
+                : new UserCookie() {
+                    Authenticated = true,
+                    Id = user.Id,
+                    Email = user.Email
+                };
+        #endregion
     }
 
-    public class UserCookie
-    {
+    public class UserCookie {
         public int Id { get; set; }
         public string Email { get; set; }
         public bool Authenticated { get; set; }
