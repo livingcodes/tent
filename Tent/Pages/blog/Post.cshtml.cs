07cfa21e643fd9cd67203534401d31b3e1b2712f
Case <case.joyce@gmail.com>
2021-05-22 23:06:10 -0500

    Upgrade to .net 5. Change blog routes. /post/{slug} /post/edit/{id}

--- Tent/Pages/blog/Post.cshtml.cs
+++ Tent/Pages/blog/Post.cshtml.cs
@@ -1,54 +1,50 @@
-using System;
+namespace Tent.Pages.blog {
+    using System;
+public class PostPage : PostBasePage {}
 
-namespace Tent.Pages.blog
-{
-    public class PostPage : PostBasePage
-    {
-    }
-    public class PostBasePage : BasePage
-    {
-        public void OnGet() {
-            if (RouteData.Values.ContainsKey("slug")) {
-                var slug = RouteData.Values["slug"].ToString();
-                var post = db.SelectOne<Post>("WHERE Slug = @Slug", slug);
-                Post = post;
-            } else if (!Request.QueryString.HasValue) {
-                var mostRecent = db.SelectOne<Post>(
-                    "ORDER BY PublishDate DESC");
-                Post = mostRecent;
-            } else {
-                var queryString = Request.QueryString.Value;
-                var id = queryString.Split('=')[1];
-                Post = db.SelectById<Post>(id.ToInt());
-            }
+public class PostBasePage : BasePage {
+    public void OnGet() {
+        if (RouteData.Values.ContainsKey("id")) {
+            var id = RouteData.Values["id"].ToStringOr("").ToInt();
+            Post = db.SelectById<Post>(id);
+        } else if (RouteData.Values.ContainsKey("slug")) {
+            var slug = RouteData.Values["slug"].ToString();
+            Post = db.SelectOne<Post>("WHERE Slug = @Slug", slug);
+        } else if (!Request.QueryString.HasValue) {
+            var mostRecent = db.SelectOne<Post>(
+                "ORDER BY PublishDate DESC");
+            Post = mostRecent;
+        } else {
+            var queryString = Request.QueryString.Value;
+            var id = queryString.Split('=')[1];
+            Post = db.SelectById<Post>(id.ToInt());
         }
+    }
 
-        public void OnPost() {
-            var save = Request.Form["save"].ToStringOr(null);
-            if (save == "Save") {
-                var html = Request.Form["html"].ToString();
-                var slug = RouteData.Values["slug"].ToString();
-                var post = db.SelectOne<Post>("WHERE Slug = @Slug", slug);
-                post.Body = html;
-                db.Update(post);
-                Post = post;
-            }
+    public void OnPost() {
+        var save = Request.Form["save"].ToStringOr(null);
+        if (save == "Save") {
+            var html = Request.Form["html"].ToString();
+            var slug = RouteData.Values["slug"].ToString();
+            var post = db.SelectOne<Post>("WHERE Slug = @Slug", slug);
+            post.Body = html;
+            db.Update(post);
+            Post = post;
+        }
             
-            var cancel = Request.Form["cancel"].ToStringOr(null);
-            if (cancel == "Cancel") {
-                Response.Redirect(Request.Path);
-            }
+        var cancel = Request.Form["cancel"].ToStringOr(null);
+        if (cancel == "Cancel") {
+            Response.Redirect(Request.Path);
         }
-
-        public Post Post { get; set; }
     }
 
-    public class Post
-    {
-        public int Id { get; set; }
-        public string Slug { get; set; }
-        public string Title { get; set; }
-        public string Body { get; set; }
-        public DateTime PublishDate { get; set; }
-    }
+    public Post Post { get; set; }
 }
+
+public class Post {
+    public int Id { get; set; }
+    public string Slug { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public DateTime PublishDate { get; set; }
+} }
--- Tent/Startup.cs
+++ Tent/Startup.cs
@@ -2,43 +2,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace Tent
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration) {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.AddScoped<Logic.ICrypto, Logic.Crypto>();
-            services
-                .AddMvc()
-                .AddRazorPagesOptions(options => {
-                    options.Conventions.AddPageRoute("/post", "/post/{slug?}");
-                });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            } else {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
-        }
+namespace Tent {
+public class Startup {
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddScoped<Logic.ICrypto, Logic.Crypto>();
+        services.AddRazorPages();
     }
-}
+
+    // configures the http request pipeline
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+        else
+            app.UseExceptionHandler("/Error");
+
+        app .UseStaticFiles()
+            .UseRouting()
+            .UseEndpoints(x => x.MapRazorPages());
+    }
+} }
